bbdf6bbdbc8c1816d0ea9786804b4ff279912fe2
Peter Elger <elger.peter@gmail.com>
2015-03-25 11:50:03 +0000

    fixed canAnalyze function

--- index.js
+++ index.js
@@ -14,6 +14,7 @@
 
 'use strict';
 
+var _ = require('lodash');
 var dockerAnalyzer = require('nscale-docker-ssh-analyzer');
 var allowedTypes = [
   'docker',
@@ -86,16 +87,15 @@ exports.analyze = function analyze(config, system, cb) {
  *
  */
 exports.canAnalyze = function canAnalyze(system) {
-
-  return system.containerDefinitions.every(function(def) {
-    var rightType = allowedTypes.indexOf(def.type) >= 0;
+  return _.some(system.topology.containers, function(cont) {
+    var rightType = allowedTypes.indexOf(cont.type) >= 0;
     var hasIp = !!(
-                  def.type === 'blank-container' &&
-                  def.specific &&
+                  cont.type === 'blank-container' &&
+                  cont.specific &&
                   (
-                    def.specific.ipAddress ||
-                    def.specific.ipaddress ||
-                    def.specific.privateIpAddress
+                    cont.specific.ipAddress ||
+                    cont.specific.ipaddress ||
+                    cont.specific.privateIpAddress
                   )
                 );
 
